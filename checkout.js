910a0e8edd00a152c8b29346d910cf324d6247de
tainasilvak <tainacunha42@gmail.com>
2023-09-04 16:25:39 -0300

    primeiro commit do projeto do intensivão de javascript da hashtag

---
+++ checkout.js
@@ -0,0 +1,40 @@
+import { 
+    desenharProdutoCarrinhoSimples, 
+    lerLocalStorage, 
+    apagarDoLocalstorage, 
+    salvarLocalStorage 
+} from "./src/utilidades";
+
+function desenharProdutosCheckout(){
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
+    for(const idProduto in idsProdutoCarrinhoComQuantidade){
+        desenharProdutoCarrinhoSimples(idProduto, "container-produtos-checkout", idsProdutoCarrinhoComQuantidade[idProduto]);
+    }
+}
+
+
+function finalizarCompra(evento){
+    evento.preventDefault();
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
+    if(Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+        return; 
+    }
+    const dataAtual = new Date();
+    const pedidoFeito = {
+        dataPedido: dataAtual,
+        pedido: idsProdutoCarrinhoComQuantidade
+    };
+
+    const historicoDePedidos = lerLocalStorage("historico") ?? [];
+    const historicoDePedidosAtualizado = [pedidoFeito, ...historicoDePedidos];
+
+    salvarLocalStorage("historico", historicoDePedidosAtualizado);
+    apagarDoLocalstorage("carrinho");
+
+    window.location.href = "./pedidos.html";
+    
+}
+
+desenharProdutosCheckout();
+
+document.addEventListener("submit", (evt) => finalizarCompra(evt));
